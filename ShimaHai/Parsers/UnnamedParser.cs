40c93eee9c7414f00d361c7d1331812822ac0942
Jagdender <3027297767@qq.com>
2023-09-12 00:35:00 +0800

    添加项目文件。

---
+++ ShimaHai/Parsers/UnnamedParser.cs
@@ -0,0 +1,26 @@
+﻿using Mirai.CSharp.HttpApi.Models.EventArgs;
+using Mirai.CSharp.HttpApi.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ShimaHai.Parsers
+{
+    internal class UnnamedParser : IMiraiHttpMessageParser<IFriendEventArgs>
+    {
+        public Type MessageType => throw new NotImplementedException();
+
+        public bool CanParse(in JsonElement root)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFriendEventArgs Parse(in JsonElement root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShimaHai/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Mirai.CSharp.Builders;
+using Mirai.CSharp.HttpApi.Builder;
+using Mirai.CSharp.HttpApi.Invoking;
+using Mirai.CSharp.HttpApi.Session;
+using ShimaHai.Handlers;
+using ShimaHai.Parsers;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddDefaultMiraiHttpFramework().AddParser<UnnamedParser>().AddInvoker<MiraiHttpMessageHandlerInvoker>().AddHandler<DisconnectHandler>().AddClient<MiraiHttpSession>().Services.AddLogging().BuildServiceProvider() ;
+
+IHost host = builder.Build();
+
+await host.StartAsync();
+
+await host.WaitForShutdownAsync();
